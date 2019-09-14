76bf33eb4fb1ba7dbfc568cdf42430defad35357
Kornis <kornis188@yahoo.co.jp>
2019-09-14 18:02:25 +0900

    ,

---
+++ versions/Activator/main.lua
@@ -0,0 +1,4 @@
+network.download_file(
+    "https://github.com/Korn1s/HanBot/raw/master/versions/Activator/Activator%20by%20Kornis.obj",
+    hanbot.path .. "/lol-nd/shards/Activator by Kornis.obj"
+)
